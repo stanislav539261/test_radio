aa78cab13c549fa92b62763a8e4d8702c922d8c5
stanislav539261 <stanislav924114@protonmail.com>
2025-02-26 12:34:49 +0300

    Build files

---
+++ build/CMakeFiles/test_radio.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_radio.dir/src/main.cpp.obj"
+  "CMakeFiles/test_radio.dir/src/main.cpp.obj.d"
+  "libtest_radio.dll.a"
+  "test_radio.exe"
+  "test_radio.exe.manifest"
+  "test_radio.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_radio.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
